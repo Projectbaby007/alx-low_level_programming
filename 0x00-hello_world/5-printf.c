7278e5b41bd339f887956b7fc856c02d06103281
Projectbaby007 <orucaleb@gmail.com>
2022-10-17 12:19:31 -0700

    c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	Return(0);
 }
