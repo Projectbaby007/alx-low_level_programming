6f269f58903ae3d80d30afde4b9f329c8c7bc8c6
Projectbaby007 <orucaleb@gmail.com>
2022-10-18 08:04:22 -0700

    variables commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print all strings from 0 to 9
+ * main - prints all strings from 0 to 9
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 		printf("%d", num)
 
 	printf("\n");
