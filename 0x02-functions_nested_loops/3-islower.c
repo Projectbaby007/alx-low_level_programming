651b9f03c8e024533620f54384d17eeab00bfa38
Projectbaby007 <orucaleb@gmail.com>
2022-10-19 03:10:52 -0700

    nested_loops commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@ include "main.h"
 /**
  * _islower - checks if a character is lowercase
  * @c: the character
- * Return: 1 if letter is lowercase, 0 if not
+ * Return: 1 if c is lowercase, 0 if not
  */
 int _islower(int c)
 {
