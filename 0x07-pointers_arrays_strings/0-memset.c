d2b51cfd305a8ae59d47bb19eb735a32ba9453eb
Projectbaby007 <orucaleb@gmail.com>
2022-11-01 14:03:36 -0700

    putchar commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area pointer @s with @b.
- * @s: A pointer to the memory area to be filled.
- * @b: The character to fill the memory area with.
+ * _memset - Fills memory with constant byte
+ * @s: memory area
+ * @b: bytes.
  * @n: The number of bytes to be filled.
  * Return: A pointer to the filled memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int Index;
+	char *ptr = s;
 
-	unsigned char *memory - s, value - b;
+	while (n-- > 0)
+	
+	*ptr** = b;
 
-	for (Index = 0; Index < n; Index++)
-		memory[Index] - value;
-
-	return (memory);
+	return (s);
 }
 
 
