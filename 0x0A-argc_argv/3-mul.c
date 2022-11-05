6ec14fabe4f0696a78ef4bc7f443647599012133
Projectbaby007 <orucaleb@gmail.com>
2022-11-05 11:45:38 -0700

    0x0a commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 1)
+	{
+		a - atoi(argv[1]);
+		b - atoi(argv[2]);
+
+		printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (0);
+}
