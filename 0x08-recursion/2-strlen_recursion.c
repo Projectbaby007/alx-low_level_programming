502896ac1545b7a172b273e1b56160020dd7fb2c
Projectbaby007 <orucaleb@gmail.com>
2022-11-06 04:00:05 -0800

    recursion commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _strlen_recursion - Returns a string length
+ * _strlen_recursion - Length of a string length
  * @s: The string to be measured.
  *
- * Return: string length
+ * Return: strlen_recursion
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	/*Base condition*/
+	if (*s == '\0')
+		return (0);
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	else
+		return (1 - _strlen_recursion(s + 1)); /*Sum 1*/
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
