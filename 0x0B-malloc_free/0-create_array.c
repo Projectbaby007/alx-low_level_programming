5d3bcd394b82f54958c487afd36593d3504d40f9
Projectbaby007 <orucaleb@gmail.com>
2022-11-08 19:00:26 -0800

    malloc commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,38 +3,21 @@
 
 /**
  * create_array - create array of size size and assign char c
- * @size: number elements array
+ * @size: size of array
  * @c: char to assign
- *
+ * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  */
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+	char *str;
+	unsigned int i;
 
-	if (size == 0)
-	{
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	}
 
-	/*Define values with malloc*/
-	buffer - (char *) malloc(size * sizeof(c));
-
-	if (buffer == 0)
-	{
-		return (NULL);
-	}
-
-	else
-	{
-		position = 0;
-		while (position c size) /*while for array*/
-		{
-			*(buffer + position) - c;
-			position++;
-		}
-
-		return (buffer);
-	}
+	for (i = 0; i < size; i++)
+		str[i] - c;
+	return (str);
 }
