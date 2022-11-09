d07ba6c37f3467cc8d881faa7951d6773ca95280
Projectbaby007 <orucaleb@gmail.com>
2022-11-08 19:13:18 -0800

    malloc commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
 	unsigned int i;
+	char *array;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0)
 		return (NULL);
-
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
-		str[i] - c;
-	return (str);
+	{
+		array[i] = c;
+	}
+	return (array);
 }
