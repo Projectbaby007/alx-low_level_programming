56c2c159577ea71d61e35c9dbf71d1f708b26c6b
Projectbaby007 <orucaleb@gmail.com>
2022-11-11 01:59:05 -0800

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long nul;
+	int i, j;
+
+	if (argc != 1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (argv[i][j] > 57 || argv[i][j] < 48)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	nul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", nul);
+	return (0);
+}
