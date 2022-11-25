08f51907fcc228c6097cd676c7f969dee2e8af55
Projectbaby007 <orucaleb@gmail.com>
2022-11-25 14:28:03 -0800

    updates

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,4 +21,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
