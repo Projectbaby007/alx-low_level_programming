7ea3016d9274e8531c9e9670edcf541c14931a77
Projectbaby007 <orucaleb@gmail.com>
2023-01-22 06:31:04 -0800

    dynamic libraries are not beans

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if c is lowercase, 0 if it is not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else
+		return (0);
+}
